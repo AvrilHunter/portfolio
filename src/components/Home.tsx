e4632ea24a6adfb76545e0f5ac1fbff212402b33
AvrilHunter <avrilhunter15@gmail.com>
2024-07-02 10:17:37 +0100

    style: about page for mobile

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,12 +2,22 @@ import photo from "../assets/headshot.jpg";
 
 function Home() {
   return (
-    <>
-      <h1 className="text-rose">Avril Hunter</h1>
-      <img className="profilephoto" src={photo} alt="profile photo of Avril" />
-      <p className="bg-violet text-white">Junior Software Developer</p>
-      <p className="bg-amber text-violet">Leeds, West Yorkshire</p>
-      <p className="bg-rose text-white">
+    <main className="flex flex-col items-center gap-1">
+      <h1 className="text-4xl text-white rounded-md bg-rose px-2 w-full">
+        Avril Hunter
+      </h1>
+      <h2 className="text-2xl bg-violet text-white rounded-md px-2 w-full">
+        Junior Software Developer
+      </h2>
+      <h3 className="text-xl bg-amber text-violet rounded-md px-2 w-full">
+        Leeds, West Yorkshire
+      </h3>
+      <img
+        className="max-w-[200px] rounded-full px-2"
+        src={photo}
+        alt="profile photo of Avril"
+      />
+      <p className="text-left justify">
         Having always relished problem-solving and logical thinking tasks, I
         have pivoted my career from procurement to software development through
         completion of the Northcoders bootcamp. Applying my technical skills to
@@ -17,7 +27,7 @@ function Home() {
         skill set that I believe will be invaluable in my new career as a
         software developer.
       </p>
-    </>
+    </main>
   );
 }
 
